f99df50611c8025b7a5394a25e9920eefa1fdc20
Cjgfree123 <cjg_free@163.com>
2019-02-24 15:15:36 +0800

    connectedRouter:将路由同步到store

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # my-app-2
 dva基础构建(自定义侧边栏配置)
+
+## 将路由信息:同步到redux里
+
+* 方式一: 使用routerRedux
+
+```
+this.props.dispatch(routerRedux.push({
+            pathname: 'url',
+            query: {id: id}
+}));
+
+const params  = location.query;
+```
+
+* 方式二: 使用connectedRouter
+
+```
+import { routerRedux } from 'dva/router';
+
+import createHashHistory from 'history/createHashHistory';
+
+const { ConnectedRouter } = routerRedux;
+
+const app = dva({
+    history: createHashHistory(),
+});
+
+<ConnectedRouter history={history}></ConnectedRouter>
+```
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
+import React from 'react';
 import dva from 'dva';
+import { Route, routerRedux, Switch } from 'dva/router';
+import createHashHistory from 'history/createHashHistory';
+
+import App from './routes/App.js';
+import Home from './routes/Home/index.js';
 import createLoading from 'dva-loading';
 import user from './models/user';
 import './index.css';
 
+const { ConnectedRouter } = routerRedux;
+
 // 1. Initialize
-const app = dva();
+const app = dva({
+    history: createHashHistory(),
+});
 app.use(createLoading());
 
 // 2. Plugins
@@ -15,7 +25,38 @@ app.use(createLoading());
 app.model(user);
 
 // 4. Router
-app.router(require('./router').default);
+function Product({ match }) {
+    if (match.path === "/product/saleIn") {
+        return <h1>商品买入</h1>;
+    };
+    if (match.path === "/product/saleOut") {
+        return <h1>商品售出</h1>;
+    };
+    return <h1>Hello 商品页</h1>;
+};
+
+function User() {
+    return <h1>Hello 用户!</h1>;
+};
+
+app.router(({ history }) => {
+    return (
+        <ConnectedRouter history={history}>
+            <App>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/home" exact component={Home} />
+                    <Route path="/product" exact component={Product} />
+                    <Route path="/product/saleIn" exact component={Product} />
+                    <Route path="/product/saleOut" exact component={Product} />
+                    <Route path="/user" exact component={User} />
+                </Switch>
+            </App>
+        </ConnectedRouter>
+    )
+})
+
+// app.router(require('./router').default);
 
 // 5. Start
 app.start('#root');
